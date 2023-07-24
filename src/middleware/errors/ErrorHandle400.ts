8514c5d7b7ec97c71509e8e20c3b8f856c8330e3
andrelrocha <andrerocha0911@gmail.com>
2023-07-24 14:29:16 -0300

    feat: middleware de tratamento de erros de req

---
+++ src/middleware/errors/ErrorHandle400.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+const errorHandle400 = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    if (typeof err === "string") {
+        return res.status(400).send(err);
+    }
+
+    if (err instanceof Error) {
+        // The error is an instance of the Error class, which has a toString() method
+        return res.status(400).send(err.toString());
+    }
+
+    return res.status(500).send({ message: "Internal Server Error" });
+};
+
+export { errorHandle400 };
